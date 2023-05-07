0e5ad23cafec13967b853af01928af65a1eaf5f3
TheDen <deniskhoshaba@gmail.com>
2023-05-08 02:50:59 +1000

    refactor functions

--- main.go
+++ main.go
@@ -17,17 +17,48 @@ const boltIconFilled = "bolt-filled.png"
 
 var lowPowerMode = ""
 
-func runCommand(str string) error {
+func getHardwareUUID() (string, error) {
+	cmd := exec.Command("system_profiler", "SPHardwareDataType")
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	lines := strings.Split(string(output), "\n")
+	for _, line := range lines {
+		if strings.Contains(line, "Hardware UUID:") {
+			uuid := strings.TrimSpace(strings.Split(line, ":")[1])
+			return uuid, nil
+		}
+	}
+	return "", fmt.Errorf("Hardware UUID not found")
+}
+
+func getPowerState(plistPath string) (uint64, uint64, error) {
+	f, err := os.Open(plistPath)
+	if err != nil {
+		return 0, 0, err
+	}
+	defer f.Close()
+	var m map[string]interface{}
+
+	// decode the plist file into the map
+	decoder := plist.NewDecoder(f)
+	err = decoder.Decode(&m)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	// extract the LowPowerMode values for Battery and AC
+	batteryLowPowerMode := m["Battery Power"].(map[string]interface{})["LowPowerMode"].(uint64)
+	acLowPowerMode := m["AC Power"].(map[string]interface{})["LowPowerMode"].(uint64)
+
+	return acLowPowerMode, batteryLowPowerMode, nil
+}
+
+func setLowPowerMode(str string) error {
 	cmd := exec.Command("/usr/bin/osascript", "-e", fmt.Sprintf("do shell script \"%s\" with prompt \"Galvani is trying to update battery prefrences\" with administrator privileges", str))
 	err := cmd.Run()
 	return err
-}
-
-func setMenuStatesFalse() {
-	menuet.Defaults().SetBoolean("alwaysState", false)
-	menuet.Defaults().SetBoolean("neverState", false)
-	menuet.Defaults().SetBoolean("batteryOnlyState", false)
-	menuet.Defaults().SetBoolean("powerOnlyState", false)
 }
 
 func checkStateAtStartup() {
@@ -77,6 +108,13 @@ func setIconState() string {
 	return boltIconOutline
 }
 
+func setMenuStatesFalse() {
+	menuet.Defaults().SetBoolean("alwaysState", false)
+	menuet.Defaults().SetBoolean("neverState", false)
+	menuet.Defaults().SetBoolean("batteryOnlyState", false)
+	menuet.Defaults().SetBoolean("powerOnlyState", false)
+}
+
 func menuItems() []menuet.MenuItem {
 	alwaysState := menuet.Defaults().Boolean("alwaysState")
 	neverState := menuet.Defaults().Boolean("neverState")
@@ -97,7 +135,7 @@ func menuItems() []menuet.MenuItem {
 	items = append(items, menuet.MenuItem{
 		Text: fmt.Sprintf("💡 Always"),
 		Clicked: func() {
-			runCommand("sudo pmset -a lowpowermode 1")
+			setLowPowerMode("sudo pmset -a lowpowermode 1")
 			setMenuStatesFalse()
 			menuet.Defaults().SetBoolean("alwaysState", true)
 		},
@@ -107,7 +145,7 @@ func menuItems() []menuet.MenuItem {
 	items = append(items, menuet.MenuItem{
 		Text: fmt.Sprintf("🛑 Never"),
 		Clicked: func() {
-			runCommand("sudo pmset -a lowpowermode 0")
+			setLowPowerMode("sudo pmset -a lowpowermode 0")
 			setMenuStatesFalse()
 			menuet.Defaults().SetBoolean("neverState", true)
 		},
@@ -117,7 +155,7 @@ func menuItems() []menuet.MenuItem {
 	items = append(items, menuet.MenuItem{
 		Text: fmt.Sprintf("🔋 Only on Battery"),
 		Clicked: func() {
-			runCommand("sudo pmset -a lowpowermode 0; sudo pmset -b lowpowermode 1")
+			setLowPowerMode("sudo pmset -a lowpowermode 0; sudo pmset -b lowpowermode 1")
 			setMenuStatesFalse()
 			menuet.Defaults().SetBoolean("batteryOnlyState", true)
 		},
@@ -127,7 +165,7 @@ func menuItems() []menuet.MenuItem {
 	items = append(items, menuet.MenuItem{
 		Text: fmt.Sprintf("🔌 Only on Power"),
 		Clicked: func() {
-			runCommand("sudo pmset -a lowpowermode 0; sudo pmset -c lowpowermode 1")
+			setLowPowerMode("sudo pmset -a lowpowermode 0; sudo pmset -c lowpowermode 1")
 			setMenuStatesFalse()
 			menuet.Defaults().SetBoolean("powerOnlyState", true)
 		},
@@ -145,47 +183,6 @@ func menu() {
 		menuet.App().MenuChanged()
 		time.Sleep(time.Second)
 	}
-}
-
-func getHardwareUUID() (string, error) {
-	cmd := exec.Command("system_profiler", "SPHardwareDataType")
-	output, err := cmd.Output()
-	if err != nil {
-		return "", err
-	}
-	lines := strings.Split(string(output), "\n")
-	for _, line := range lines {
-		if strings.Contains(line, "Hardware UUID:") {
-			uuid := strings.TrimSpace(strings.Split(line, ":")[1])
-			return uuid, nil
-		}
-	}
-	return "", fmt.Errorf("Hardware UUID not found")
-}
-
-func getPowerState(plistPath string) (uint64, uint64, error) {
-	// open the plist file
-	f, err := os.Open(plistPath)
-	if err != nil {
-		return 0, 0, err
-	}
-	defer f.Close()
-
-	// create a map to store the unmarshaled data
-	var m map[string]interface{}
-
-	// decode the plist file into the map
-	decoder := plist.NewDecoder(f)
-	err = decoder.Decode(&m)
-	if err != nil {
-		return 0, 0, err
-	}
-
-	// extract the LowPowerMode values for Battery and AC
-	batteryLowPowerMode := m["Battery Power"].(map[string]interface{})["LowPowerMode"].(uint64)
-	acLowPowerMode := m["AC Power"].(map[string]interface{})["LowPowerMode"].(uint64)
-
-	return acLowPowerMode, batteryLowPowerMode, nil
 }
 
 func main() {
