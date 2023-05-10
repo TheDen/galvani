eedbbb8b52e42e4c9803e1f01f77d279dbace83f
TheDen <deniskhoshaba@gmail.com>
2023-05-10 23:11:08 +1000

    cleanup

--- generate_icons.sh
+++ generate_icons.sh
@@ -6,10 +6,6 @@ cd "$(dirname "${0}")" || exit
 rm -rf "output_path/*" || true
 
 for size in 16 32 64 128 256 512 1024; do
-  #  half="$((size / 2))"
-  #  echo $half
-  #  convert icon.png -resize x$size $output_path/icon_${size}x${size}.png
-  #  convert icon.png -resize x$size $output_path/icon_${half}x${half}@2x.png
   sips -z $size $size ${icon_name} --out "${output_path}/icon_${size}x${size}.png"
   sips -z $size $size ${icon_name} --out "${output_path}/icon_${size}x${size}@2x.png"
 done
